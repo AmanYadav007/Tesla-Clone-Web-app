1a93c80af0b28c87836f0694a8988ac8946ecadd
Aman Yadav <58115487+AmanYadav007@users.noreply.github.com>
2021-09-25 18:36:11 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+// import Header  from './Components/Header';
+import Home from './Components/Home';
+// import Section from './Components/Section';
 
 function App() {
   return (
     <div className="App">
-      HI
+      {/* <Header /> */}
+      <Home />
+      {/* <Section /> */}
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
 
 function Home() {
     return (
         <div>
-            Home
+                <Container>
+                    <Section />
+                </Container>
         </div>
     )
 }
 
 export default Home
+
+
+const Container = styled.div`
+    height: 100vh;
+`
